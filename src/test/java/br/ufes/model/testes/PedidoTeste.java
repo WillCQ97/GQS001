4aae1a25bb5b549a94f74b05969ca2b1a93d7588
WillCQ97 <willian.cqueiroz@gmail.com>
2020-09-24 14:32:09 -0300

    Implementação CT02 PedidoTeste

--- src/test/java/br/ufes/model/testes/PedidoTeste.java
+++ src/test/java/br/ufes/model/testes/PedidoTeste.java
@@ -21,22 +21,22 @@ import org.junit.Test;
  * @author willcq
  */
 public class PedidoTeste {
-    
+
     public PedidoTeste() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -57,8 +57,30 @@ public class PedidoTeste {
 
         double valorTotalEsperado = canetaAzul.getValorUnitario()
                 * qtdAzul + canetaVermelha.getValorUnitario() * qtdVerm;
-        
+
         Assert.assertEquals(valorTotalEsperado, pedido.getValor(), 0.001);
+
+    }
+
+    @Test
+    public void CT02() {
+        Produto canetaAzul = new Produto("Caneta Azul", 1.5, 100);
+        Produto canetaVermelha = new Produto("Caneta Vermelha", 1.6, 50);
+        int qtdAzul = 3;
+        int qtdVerm = 2;
+
+        Pedido pedido = new Pedido(
+                new Cliente("Fulano da Silva", "123.456.789-01"),
+                canetaAzul,
+                qtdAzul, LocalDate.now());
+
+        pedido.addItem(canetaVermelha, qtdVerm);
+
+        double valorTotalPedido = canetaAzul.getValorUnitario()
+                * qtdAzul + canetaVermelha.getValorUnitario() * qtdVerm;
         
+        double valorDescontoEsperado = valorTotalPedido * 0.05;
+        
+        Assert.assertEquals(valorDescontoEsperado, pedido.getValorDesconto(), 0.001);
     }
 }
